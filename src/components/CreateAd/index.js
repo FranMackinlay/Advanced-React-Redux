c241380b7cf1fdf7edebcf3419ba7c594cc4ec35
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-27 19:36:57 -0300

    createAd refactor & redux

---
+++ src/components/CreateAd/index.js
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux';
+
+import CreateAd from './CreateAd';
+
+import { adCreation } from '../../store/actions';
+import { getVisibleAds } from '../../store/selectors';
+
+
+function mapStateToProps(state, ownProps) {
+  return {
+    ads: getVisibleAds(state),
+  };
+}
+
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    adCreation: adToCreate => dispatch(adCreation(adToCreate)),
+  };
+}
+
+// const mapDispatchToProps = {
+//   adCreation: adToCreate => dispatch(adCreation(adToCreate)),
+// };
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateAd);
