b8b5835b57a7c59fd439651b6d26622c0ba3c764
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-27 18:26:24 -0300

    create and edit ad type created

--- src/store/types.js
+++ src/store/types.js
@@ -1,3 +1,7 @@
 export const FETCH_ADS_REQUEST = 'FETCH_ADS_REQUEST';
 export const FETCH_ADS_SUCCESS = 'FETCH_ADS_SUCCESS';
 export const FETCH_ADS_FAILURE = 'FETCH_ADS_FAILURE';
+
+export const EDIT_AD = 'EDIT_AD';
+
+export const CREATE_AD = 'CREATE_AD';
