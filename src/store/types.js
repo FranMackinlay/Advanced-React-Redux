b348683440e0ce0f864b5e76f4026696fc76d44d
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-28 19:51:57 -0300

    userLogin and checkLocal

--- src/store/types.js
+++ src/store/types.js
@@ -5,3 +5,9 @@ export const FETCH_ADS_FAILURE = 'FETCH_ADS_FAILURE';
 export const EDIT_AD = 'EDIT_AD';
 
 export const CREATE_AD = 'CREATE_AD';
+
+export const USER_LOGIN = 'USER_LOGIN';
+export const USER_LOGOUT = 'USER_LOGOUT';
+
+export const CHECK_LOCAL = 'CHECK_LOCAL';
+export const SET_LOCAL = 'SET_LOCAL';
