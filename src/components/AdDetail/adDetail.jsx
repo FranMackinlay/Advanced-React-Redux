b5cd571616776154a75c06d938923b2e749b758f
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-27 21:13:16 -0300

    mini refactor

--- src/components/AdDetail/adDetail.jsx
+++ src/components/AdDetail/adDetail.jsx
@@ -1,73 +1,66 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import './adDetail.css';
 
 const { getAdDetail } = api();
 
-
 export default class adDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ad: {},
-      tags: []
-    }
-    const id = this.props.match.params._id;
-    this.getDetailAd(id);
+	constructor(props) {
+		super(props);
+		this.state = {
+			ad: {},
+			tags: [],
+		};
+	}
 
-  }
+	componentDidMount() {
+		const id = this.props.match.params._id;
+		this.getDetailAd(id);
+	}
 
-  getDetailAd = async (id) => {
-    const detailAd = await getAdDetail(id);
-    if (detailAd.error) {
-      alert('No se ha podido encontrar el detalla de este anuncio');
-      this.props.history.push('/anuncios');
-    } else {
-      this.setState({
-        ad: detailAd.result,
-        tags: detailAd.result.tags
-      })
-      return detailAd.result;
-    }
+	getDetailAd = async id => {
+		const detailAd = await getAdDetail(id);
+		if (detailAd.error) {
+			alert('No se ha podido encontrar el detalla de este anuncio');
+			this.props.history.push('/anuncios');
+		} else {
+			this.setState({
+				ad: detailAd.result,
+				tags: detailAd.result.tags,
+			});
+			return detailAd.result;
+		}
+	};
 
-  }
+	render() {
+		const { ad, tags } = this.state;
 
+		const adTagsArray = tags.toString();
 
-  render() {
-    const { ad, tags } = this.state;
+		const adTags = adTagsArray.replace(/,/g, ' - ');
 
-    const adTagsArray = tags.toString();
-
-    const adTags = adTagsArray.replace(/,/g, ' - ');
-    console.log(typeof adTagsArray);
-
-    // console.log(ad.tags[0]);
-    // const tags = ad.tags.replace(',', ' ');
-    // console.log(tags)
-
-    return (
-      <div>
-        <Link className="back-to-ads" to="/anuncios">
-          <button className="back-btn">Atras</button>
-        </Link>
-        <div className="detail-container">
-          <img className="detail-image" src={ad.photo} alt="AdImage" />
-          <h1>{ad.name}</h1>
-          <div className="price-type">
-            <p>Price: {ad.price}</p>
-            <p>Type: {ad.type}</p>
-          </div>
-          <p>Description: {ad.description}</p>
-          <p>Tags: {adTags}</p>
-          <br />
-          <Link to={`/editAd/id=${ad._id}`}>
-            <button className="edit-ad">Editar Anuncio</button>
-          </Link>
-          <br />
-
-        </div >
-      </div>
-    )
-  }
+		return (
+			<div>
+				<Link className='back-to-ads' to='/anuncios'>
+					<button className='back-btn'>Atras</button>
+				</Link>
+				<div className='detail-container'>
+					<img className='detail-image' src={ad.photo} alt='AdImage' />
+					<h1>{ad.name}</h1>
+					<div className='price-type'>
+						<p>Price: {ad.price}</p>
+						<p>Type: {ad.type}</p>
+					</div>
+					<p>Description: {ad.description}</p>
+					<p>Tags: {adTags}</p>
+					<br />
+					<Link to={`/editAd/id=${ad._id}`}>
+						<button className='edit-ad'>Editar Anuncio</button>
+					</Link>
+					<br />
+				</div>
+			</div>
+		);
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './components/App/App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
@@ -22,7 +9,6 @@ import { configureStore } from './store';
 
 const history = createBrowserHistory();
 const store = configureStore({ api, history })();
-console.log('STORE', store);
 ReactDOM.render(
   <Root store={store} history={history} />,
   document.getElementById('root')
