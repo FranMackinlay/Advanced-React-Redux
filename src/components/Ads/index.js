3a740e8e7d975aea8cf8404407fcd7654e866871
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-28 19:53:52 -0300

    ads index

--- src/components/Ads/index.js
+++ src/components/Ads/index.js
@@ -2,11 +2,14 @@ import { connect } from 'react-redux';
 
 import Ads from './Ads';
 
-import { getVisibleAds } from '../../store/selectors';
+import { getVisibleAds, localGetter } from '../../store/selectors';
 
 function mapStateToProps(state, ownProps) {
+  console.log('STATE', state);
   return {
     ads: getVisibleAds(state),
+    localCheck: localGetter(state),
   };
 }
+
 export default connect(mapStateToProps)(Ads);
