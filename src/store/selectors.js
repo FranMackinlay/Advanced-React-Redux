68472eb370c32aa7b484ca78bfb4fb36f2f1524d
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-28 19:52:20 -0300

    isLogged selectors

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,7 +1,23 @@
+
 export const getAds = state => state.ads;
+export const getUserLogin = state => state.checkLogin;
+export const getLocalCheck = state => state.localChecking;
 
 export function getVisibleAds(state) {
   const ads = getAds(state);
   const visibleAds = ads;
   return visibleAds;
+};
+
+export function isUserLoggedIn(state) {
+  console.log('SELECTOR', state);
+  const isLogged = getUserLogin(state);
+  return isLogged;
+};
+
+export function localGetter(state) {
+  console.log('LOCAL GETTER STATE', state);
+  const local = getLocalCheck(state);
+  console.log('LOCAL', local);
+  return local;
 }
