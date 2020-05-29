45b0009ee3fe79949fdbbcc4ff96b425f30858ea
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-29 19:52:13 -0300

    async test finished

--- src/store/actions.test.js
+++ src/store/actions.test.js
@@ -1,7 +1,31 @@
 import * as actions from './actions';
 import * as TYPES from './types';
-import api from '../services/api';
-jest.mock('../services/api.js');
+
+describe('Accion asincrona: fetchAds', () => {
+  test('should dispatch FETCH_ADS_REQUEST and FETCH_ADS_SUCCESS actions', async () => {
+    const ads = [];
+    const getAds = jest.fn().mockResolvedValue({ results: ads });
+    const dispatch = jest.fn();
+    const action = actions.fetchAds();
+    const getState = () => { };
+    const api = () => ({
+      getAds,
+    });
+
+    await action(dispatch, getState, { api });
+
+    expect(dispatch).toHaveBeenNthCalledWith(1, {
+      type: TYPES.FETCH_ADS_REQUEST,
+    });
+
+    expect(getAds).toHaveBeenCalled();
+
+    expect(dispatch).toHaveBeenNthCalledWith(2, {
+      type: TYPES.FETCH_ADS_SUCCESS,
+      ads,
+    });
+  });
+});
 
 describe('Accion sincrona: loginUserAction', () => {
   test('should create one USER_LOGIN action with success', () => {
@@ -13,28 +37,5 @@ describe('Accion sincrona: loginUserAction', () => {
     expect(actions.loginUserAction(success)).toEqual(
       expectedAction
     );
-  });
-});
-
-describe('Accion asincrona: fetchBikes', () => {
-  test('should dispatch FETCH_ADS_REQUEST and FETCH_ADS_SUCCESS actions', async () => {
-    const dispatch = jest.fn();
-    const action = actions.fetchAds();
-    const getState = () => { };
-    const ads = [];
-    api.getAds.mockResolvedValue(ads);
-
-    await action(dispatch, getState);
-
-    expect(dispatch).toHaveBeenNthCalledWith(1, {
-      type: TYPES.FETCH_ADS_REQUEST,
-    });
-
-    expect(api.getAds).toHaveBeenCalled();
-
-    expect(dispatch).toHaveBeenNthCalledWith(2, {
-      type: TYPES.FETCH_ADS_SUCCESS,
-      ads,
-    });
   });
 });
