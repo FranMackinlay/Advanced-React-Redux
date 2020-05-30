54139525cab78e32e3f950712b41d99120f833b0
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-30 12:20:12 -0300

    list component created

---
+++ src/components/List/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function List({ items, renderItem }) {
+	return (
+		<ul>
+			{items.map(item => (
+				<li key={item._id}>{renderItem(item)}</li>
+			))}
+		</ul>
+	);
+}
