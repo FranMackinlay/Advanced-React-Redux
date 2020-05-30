63d5c3d6a2ae08d01ec3f0ee2d9380b6abb1ae1e
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-30 12:19:50 -0300

    Login refactor

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -4,18 +4,22 @@ import Form from '../Form/Form';
 import './login.css';
 
 export default function Login(props) {
-	const { info } = useForm();
+	const { info, setInfo } = useForm();
 
 	const onSubmit = async event => {
 		event.preventDefault();
 		console.log(info);
 		await props.userLogin(info);
-	};
-	useEffect(() => {
 		if (props.isUserLoggedIn) {
 			props.history.push('/anuncios');
+			setInfo({
+				username: '',
+				password: '',
+			});
 		}
-	}, [props]);
+	};
+
+	useEffect(() => {}, [props]);
 
 	return (
 		<div className='login'>
